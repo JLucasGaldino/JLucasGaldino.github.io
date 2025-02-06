726338e8c5acb83156ef61fd9122b4fffeee7822
JLucasGaldino <99841302+JLucasGaldino@users.noreply.github.com>
2025-02-06 00:17:29 +0000

    Deploying to gh-pages from @ JLucasGaldino/JLucasGaldino.github.io@c83b8bd13c385ae9c24e3e4b7fa94dc91b3fdb5e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -450,14 +450,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%67%65%6E%65%72%61%6C@%6C%75%63%61%73%67%61%6C%64%69%6E%6F.%61%6E%6F%6E%61%64%64%79.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/JLucasGaldino", "_blank");
         },
       },{
         id: 'social-rss',
@@ -467,18 +467,18 @@ ninja.data = [{
           window.open("/feed.xml", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
+        id: 'social-x',
+        title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://twitter.com/_lucas_galdino", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-youtube',
+        title: 'YouTube',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://youtube.com/@JLucasGaldino", "_blank");
         },
       },{
       id: 'light-theme',
